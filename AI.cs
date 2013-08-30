d0191cff2897cf0f266185d84c94ad5f199fa519
G-J van Rooyen <gvrooyen@sun.ac.za>
2013-08-30 17:22:29 +0200

    Worked on the GunAndRun AI mode.

--- AI.cs
+++ AI.cs
@@ -103,6 +103,119 @@ namespace battlecity
             else
                 return MoveDirection(direction);
         }
+		
+		protected ChallengeService.action RunAndGun(Tank tank, int destX, int destY)
+		{
+			/* Move towards the destination coordinates (L-shaped path, longest edge first).
+			 * If any obstructions are found en route, destroy them.
+			 * 
+			 * The function just returns the next move required in order to act on this
+			 * strategy. The path is searched along the longest edge, to determine whether
+			 * any obstructions exist along that path. If not, the tank simply moves in that
+			 * direction.
+			 * 
+			 * For the first obstruction found along the path, the tank will attempt to move
+			 * laterally to come in line with the obstruction. Of course, that movement may
+			 * come across obstacles as well, which must be removed first, possibly requiring
+			 * another lateral movement. This search is done recursively until a valid move
+			 * or firing direction is found.
+			 * 
+			 * It is possible for tanks to get trapped, e.g. in the following map:
+			 *
+			 * ·········
+             * ··#···#··
+             * ·#XXXXX#·
+             * ··XXXXX··
+             * ··XXXXO··
+             * ··XXXXX··
+             * ·#XXXXX#·
+             * ··#···#··
+             * ·········
+             * 
+             * No combination of movement or firing can let the tank move. However, this
+             * condition can only occur if the tank is placed in such a position at the
+             * start of the game -- if you can move in somewhere, you can move out again.
+             * 
+             * Nonetheless, this strategy should detect if no combination of actions can get
+             * the tank to the desired destination, and return NONE as resulting action.
+			 */
+			
+			int Lx = destX - tank.x;
+			int Ly = destX - tank.y;
+			int dx = 0;
+			int dy = 0;
+			int[] scanOrder = {0,1,-1,-2,2};
+			
+			if ((Lx == 0) && (Ly == 0))
+				return ChallengeService.action.NONE;
+			
+			if (Math.Abs(Lx) > Math.Abs(Ly))
+				dx = Math.Sign(Lx);
+		    else
+				dy = Math.Sign(Ly);
+			
+			
+			// Do a search along the 5-blocks-wide path for obstacles
+			
+			int obstX = -1;
+			int obstY = -1;
+			
+			for (int x = tank.x + dx*3, y = tank.y + dy*3; 
+			     (x <= destX + dx*2) && (destY <= destY + dy*2);
+			     x += dx, y += dy)
+			{
+				/* Depending on whether we're scanning horizontally or vertically, we look for
+				 * obstacles in a vertical or horizontal line. We start looking for obstacles at
+				 * the center point first, because these are easiest to destroy (and will take out
+				 * any neighbouring blocks).
+				 */
+				
+				if (dx == 0)
+				{
+					// We're running vertically, so look for obstacles in a horizontal line.
+					foreach (int i in scanOrder)
+					{
+						if (board.board[x+scanOrder[i]][y] == ChallengeService.state.FULL)
+						{
+							obstX = x + scanOrder[i];
+							obstY = y;
+							break;
+						}
+					}
+				}
+				else
+				{
+					// We're running horizontally, so look for obstacles in a vertical line.
+					foreach (int i in scanOrder)
+					{
+						if (board.board[x][y+scanOrder[i]] == ChallengeService.state.FULL)
+						{
+							obstX = x;
+							obstY = y + scanOrder[i];
+							break;
+						}
+					}
+				}
+				
+			}
+						
+			if ((obstX == -1) || (obstY == -1))
+				// The path's clear!
+				if (dx == -1)
+					return ChallengeService.action.LEFT;
+				else if (dx == 1)
+					return ChallengeService.action.RIGHT;
+				else if (dy == -1)
+					return ChallengeService.action.UP;
+				else if (dy == 1)
+					return ChallengeService.action.DOWN;
+				else
+					return ChallengeService.action.NONE;
+			
+			if (((dx == 0) && (obstY == tank.y)) || (dy == 0) && (obstX == tank.x))
+				// The next obstacle is perfectly lined up with the tank. If we're facing
+				// the right direction, fire. Otherwise, move into the right direction
+		}
 
     }
 
